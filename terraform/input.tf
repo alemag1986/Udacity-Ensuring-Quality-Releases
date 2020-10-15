a60f1088fbe199a216336a89db58a504d9b3d165
Alejandro Magnani <alejandro.magnani@gmail.com>
2020-10-15 16:39:59 -0500

    removed inputs

--- terraform/input.tf
+++ terraform/input.tf
@@ -1,9 +1,3 @@
-# Azure GUIDS
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
-
 # Resource Group/Location
 variable "location" {}
 variable "resource_group" {}

abe2413c8209ce205249a7ef3894d4614cb1fee3
Alejandro Magnani <alejandro.magnani@gmail.com>
2020-10-16 16:54:13 -0500

    terraform

--- terraform/modules/network/network.tf
+++ terraform/modules/network/network.tf
@@ -8,5 +8,5 @@ resource "azurerm_subnet" "test" {
   name                 = "${var.resource_type}SUB-${var.application_type}"
   resource_group_name  = var.resource_group
   virtual_network_name = azurerm_virtual_network.test.name
-  address_prefixes     = ["${var.address_prefix_test}"]
+  address_prefixes     = [ var.address_prefix_test ]
 }

b77ad99a495b808a0b1fe0bad7a4e40a0755f8ff
Alejandro Magnani <alejandro.magnani@gmail.com>
2020-10-16 15:16:56 -0500

    ip

--- terraform/modules/publicip/publicip.tf
+++ terraform/modules/publicip/publicip.tf
@@ -1,7 +1,7 @@
 resource "azurerm_public_ip" "test" {
   name                = "${var.resource_type}-${var.application_type}"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group}"
+  location            = var.location
+  resource_group_name = var.resource_group
   allocation_method   = "Dynamic"
 
 }

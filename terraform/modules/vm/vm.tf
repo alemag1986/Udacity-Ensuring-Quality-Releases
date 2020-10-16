780347f7c82d0861c1377c8160cec3ac2eb0d5ae
Alejandro Magnani <alejandro.magnani@gmail.com>
2020-10-16 15:04:42 -0500

    keys

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -24,7 +24,7 @@ resource "azurerm_linux_virtual_machine" "test" {
   
   admin_ssh_key {
     username   = var.admin_username
-    public_key = "file("~/.ssh/id_rsa.pub")"
+    public_key = "file("/home/vsts/work/_temp/id_rsa")"
   }
   os_disk {
     caching           = "ReadWrite"

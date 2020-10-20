3258093abd5b34b913d92d07b7c530e833af8b8a
Alejandro Magnani <alejandro.magnani@gmail.com>
2020-10-20 13:47:35 -0500

    change in selenium

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -7,10 +7,9 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 def login (user, password):
     print ('Starting the browser...')
     # --uncomment when running in Azure DevOps.
-    # options = ChromeOptions()
-    # options.add_argument("--headless") 
-    # driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome()
+    options = ChromeOptions()
+    options.add_argument("--headless") 
+    driver = webdriver.Chrome(options=options)
     print ('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
 
